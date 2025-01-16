f3e4d1131dbbb5cab91c0fb0afd04c9fdfb6a741
Daria Eliseeva <elisey422das@gmail.com>
2025-01-16 17:45:15 +0100

    add images

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import AboutSection from './components/AboutSection.vue'
 import HeaderSection from './components/HeaderSection.vue'
 </script>
 
 <template>
   <HeaderSection></HeaderSection>
+  <AboutSection></AboutSection>
   <!-- We don't need router yet -->
   <!-- <RouterView /> -->
 </template>
