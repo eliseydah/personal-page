b86535bfa58bafba6c41536b630b6cdea659d8ba
Daria Eliseeva <elisey422das@gmail.com>
2025-01-17 23:04:45 +0100

    fix card component issues

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -3,10 +3,17 @@
     <div class="description">
       <img :src="getImageUrl()" class="svg-icon" :alt="title" width="50" height="60" />
       <h3>{{ title }}</h3>
+    </div>
+    <div>
       <p>{{ description }}</p>
     </div>
-    <div class="forLink">
-      <a :href="link">go to {{ title }}</a>
+    <div class="links">
+      <a v-if="linkJs" :href="linkJs">
+        <img src="../assets/icons/javascript.svg" width="25" height="25" />
+      </a>
+      <a v-if="linkVue" :href="linkVue">
+        <img src="../assets/icons/vue.svg" width="25" height="25"
+      /></a>
     </div>
   </div>
 </template>
@@ -25,17 +32,61 @@ const props = defineProps({
     type: String,
     required: true,
   },
-  link: {
+  linkJs: {
     type: String,
-    required: true,
+    required: false,
+  },
+  linkVue: {
+    type: String,
+    required: false,
   },
 })
 
 function getImageUrl() {
-  return new URL(`../assets/icons/${props.iconFilename}`, import.meta.url)
+  return new URL(`../assets/icons/${props.iconFilename}`, import.meta.url).toString()
 }
 </script>
 
 <style scoped>
-/* Add your styles here */
+.card {
+  background-color: rgba(240, 248, 255, 0.5);
+  padding: 1rem 1rem 0.5rem;
+  display: flex;
+  justify-content: space-between;
+  gap: 1rem;
+  flex-direction: column;
+}
+h3 {
+  font-weight: 500;
+}
+.links {
+  display: flex;
+  justify-content: end;
+  align-items: end;
+}
+h3 {
+  align-self: center;
+}
+.description {
+  display: flex;
+  flex-direction: row;
+  justify-content: baseline;
+  gap: 1rem;
+}
+a {
+  color: darkblue;
+  text-decoration: none;
+  padding: 0.5rem 1rem;
+  font-weight: 200;
+  font-style: italic;
+}
+
+a:hover {
+  background-color: rgba(240, 248, 255, 0.495);
+  border-radius: 10px;
+}
+
+.svg-icon {
+  filter: invert(27%) sepia(100%) saturate(549%) hue-rotate(180deg) brightness(75%) contrast(101%);
+}
 </style>
--- src/components/ServicesSection.vue
+++ src/components/ServicesSection.vue
@@ -1,5 +1,52 @@
 <script setup lang="ts">
 import CardComponent from './CardComponent.vue'
+const cards = [
+  {
+    iconFilename: 'list.svg',
+    title: 'To-do list',
+    description:
+      'A simple application for managing task lists. Allows you to add, delete and mark tasks as completed.',
+    linkJS: 'https://github.com/eliseydah/todo-js/tree/master/todo',
+    linkVue: 'https://github.com/eliseydah/todo-js/tree/master/vue-todo',
+  },
+  {
+    iconFilename: 'message.svg',
+    title: 'Chat page',
+    description:
+      'A simple application that simulates chat functionality, showcasing only the frontend without any backend integration.',
+    linkJS: 'https://github.com/eliseydah/todo-js/tree/master/chat',
+    linkVue: 'https://github.com/eliseydah/todo-js/tree/master/vue-chat',
+  },
+  {
+    iconFilename: 'wheel.svg',
+    title: 'Wheel of fortune',
+    description:
+      'This is interactive web application where users can spin a Wheel of Fortune to receive a name of the city as an idea to visit.',
+    linkJS: 'https://github.com/eliseydah/todo-js/tree/master/fortune',
+  },
+  {
+    iconFilename: 'wish.svg',
+    title: 'Wishlist',
+    description:
+      'A simple application for creating a list of wishes. It allows you to create a wish card, describe a wish, upload a picture, add a link, and delete the wish card.',
+    linkJS: 'https://github.com/eliseydah/todo-js/tree/master/wishlist',
+    linkVue: 'https://github.com/eliseydah/todo-js/tree/master/vue-wishlist',
+  },
+  {
+    iconFilename: 'weather.svg',
+    title: 'Weather widget',
+    description: 'A simple web-page which allows to search a weather.',
+    linkJS: 'https://github.com/eliseydah/todo-js/tree/master/weather',
+    linkVue: 'https://github.com/eliseydah/todo-js/tree/master/vue-weather-widget',
+  },
+  {
+    iconFilename: 'layout.svg',
+    title: 'A layout training',
+    description:
+      'A simple application for creating a list of wishes. It allows you to create a wish card, describe a wish, upload a picture, add a link, and delete the wish card.',
+    linkJS: 'https://github.com/eliseydah/todo-js/tree/master/landing',
+  },
+]
 </script>
 <template>
   <section>
@@ -7,105 +54,16 @@ import CardComponent from './CardComponent.vue'
   </section>
   <div class="card-container">
     <CardComponent
-      iconFilename="message.svg"
-      title="To-do list"
-      description="A simple application for managing task lists. Allows you to add, delete and mark tasks as completed."
-      link="https://github.com/eliseydah/todo-js/tree/master/todo"
+      v-for="card in cards"
+      :key="card.title"
+      :title="card.title"
+      :description="card.description"
+      :iconFilename="card.iconFilename"
+      :link-js="card.linkJS"
+      :link-vue="card.linkVue"
     />
-    <div class="card">
-      <div class="description">
-        <img src="../assets/icons/message.svg" class="svg-icon" alt="" width="50" height="60" />
-        <h3>Chat page</h3>
-        <p>
-          A simple application that simulates chat functionality, showcasing only the frontend
-          without any backend integration.
-        </p>
-      </div>
-      <div class="forLink">
-        <a href="https://github.com/eliseydah/todo-js/tree/master/chat">go to Chat page</a>
-      </div>
-    </div>
-    <div class="card">
-      <div class="description">
-        <img src="./forest.jpg" alt="" width="50" height="60" />
-        <h3>Wheel of fortune</h3>
-        <p>
-          This is interactive web application where users can spin a Wheel of Fortune to receive a
-          name of the city as an idea to visit.
-        </p>
-      </div>
-      <div class="forLink">
-        <a href="https://github.com/eliseydah/todo-js/tree/master/fortune"
-          >go to Wheel of fortune</a
-        >
-      </div>
-    </div>
 
-    <div class="card">
-      <div class="description">
-        <img src="./forest.jpg" alt="" width="50" height="60" />
-        <h3>Wishlist</h3>
-        <p>
-          A simple application for creating a list of wishes. It allows you to create a wish card,
-          describe a wish, upload a picture, add a link, and delete the wish card.
-        </p>
-      </div>
-      <div class="forLink">
-        <a href="https://github.com/eliseydah/todo-js/tree/master/wishlist"> go to wishlist</a>
-      </div>
-    </div>
-    <div class="card">
-      <div class="description">
-        <img src="./forest.jpg" alt="" width="50" height="60" />
-        <h3>Weather widget</h3>
-        <p>A simple web-page which allows to search a weather.</p>
-      </div>
-      <div class="forLink">
-        <a href="https://github.com/eliseydah/todo-js/tree/master/weather">go to the vue chat</a>
-      </div>
-    </div>
-    <div class="card">
-      <div class="description">
-        <img src="./forest.jpg" alt="" width="50" height="60" />
-        <h3>A layout training</h3>
-        <p>
-          A simple application for creating a list of wishes. It allows you to create a wish card,
-          describe a wish, upload a picture, add a link, and delete the wish card.
-        </p>
-      </div>
-      <div class="forLink">
-        <a href="https://github.com/eliseydah/todo-js/tree/master/landing"> to layout</a>
-      </div>
-    </div>
-    <div class="card">
-      <div class="description">
-        <img src="./forest.jpg" alt="" width="50" height="60" />
-        <h3>Vue to-do list</h3>
-        <p>
-          A simple application for managing task lists. Allows you to add, delete and mark tasks as
-          completed.
-        </p>
-      </div>
-      <div class="forLink">
-        <a href="https://github.com/eliseydah/todo-js/tree/master/vue-todo">
-          go to vue to-do list</a
-        >
-      </div>
-    </div>
-    <div class="card">
-      <div class="description">
-        <img src="./forest.jpg" alt="" width="50" height="60" />
-        <h3>Vue chat</h3>
-        <p>
-          A simple application that simulates chat functionality, showcasing only the frontend
-          without any backend integration.
-        </p>
-      </div>
-      <div class="forLink">
-        <a href="https://github.com/eliseydah/todo-js/tree/master/vue-chat"> go to the vue chat</a>
-      </div>
-    </div>
-    <div class="card">
+    <!-- <div class="card">
       <div class="description">
         <img src="./forest.jpg" alt="" width="50" height="60" />
         <h3>Vue Wishlist</h3>
@@ -131,7 +89,7 @@ import CardComponent from './CardComponent.vue'
           go to vue weather widget</a
         >
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 <style scoped>
@@ -143,37 +101,5 @@ import CardComponent from './CardComponent.vue'
   /* justify-content: space-around; */
   gap: 1rem;
   margin-top: 1rem;
-}
-.card {
-  background-color: rgba(240, 248, 255, 0.35);
-  padding: 1rem 1rem 0.5rem;
-  display: flex;
-  justify-content: space-between;
-  gap: 1rem;
-  flex-direction: column;
-}
-h3 {
-  font-weight: 500;
-}
-.forLink {
-  display: flex;
-  justify-content: end;
-  align-items: end;
-}
-a {
-  color: darkblue;
-  text-decoration: none;
-  padding: 0.5rem 1rem;
-  font-weight: 200;
-  font-style: italic;
-}
-
-a:hover {
-  background-color: rgba(240, 248, 255, 0.295);
-  border-radius: 10px;
-}
-
-.svg-icon {
-  filter: invert(27%) sepia(100%) saturate(7496%) hue-rotate(180deg) brightness(95%) contrast(101%);
 }
 </style>
