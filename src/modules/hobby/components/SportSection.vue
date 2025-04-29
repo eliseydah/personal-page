2c63437c7e68c421ad19ecec0e0ee9607f16f0b9
Daria Eliseeva <elisey422das@gmail.com>
2025-04-29 14:56:08 +0200

    feat: add photo for sport section

--- src/modules/hobby/components/SportSection.vue
+++ src/modules/hobby/components/SportSection.vue
@@ -4,44 +4,28 @@ import SignComponent from '@/modules/shared/components/SignComponent.vue'
 import TextCard from '@/modules/shared/components/TextCard.vue'
 const photos = [
   {
-    url: new URL(`../../../assets/urban/01.jpg`, import.meta.url).toString(),
+    url: new URL(`../../../assets/sport/1.jpg`, import.meta.url).toString(),
     id: '01',
   },
   {
-    url: new URL(`../../../assets/urban/02.jpg`, import.meta.url).toString(),
+    url: new URL(`../../../assets/sport/2.jpg`, import.meta.url).toString(),
     id: '02',
   },
   {
-    url: new URL(`../../../assets/urban/05.jpg`, import.meta.url).toString(),
+    url: new URL(`../../../assets/sport/3.jpg`, import.meta.url).toString(),
     id: '03',
   },
   {
-    url: new URL(`../../../assets/urban/04.jpg`, import.meta.url).toString(),
+    url: new URL(`../../../assets/sport/4.jpg`, import.meta.url).toString(),
     id: '04',
   },
   {
-    url: new URL(`../../../assets/urban/06.jpg`, import.meta.url).toString(),
+    url: new URL(`../../../assets/sport/5.jpg`, import.meta.url).toString(),
     id: '05',
   },
   {
-    url: new URL(`../../../assets/urban/07.jpg`, import.meta.url).toString(),
+    url: new URL(`../../../assets/sport/6.jpg`, import.meta.url).toString(),
     id: '06',
-  },
-  {
-    url: new URL(`../../../assets/urban/08.jpg`, import.meta.url).toString(),
-    id: '07',
-  },
-  {
-    url: new URL(`../../../assets/urban/01.jpg`, import.meta.url).toString(),
-    id: '08',
-  },
-  {
-    url: new URL(`../../../assets/urban/09.jpg`, import.meta.url).toString(),
-    id: '09',
-  },
-  {
-    url: new URL(`../../../assets/urban/04.jpg`, import.meta.url).toString(),
-    id: '10',
   },
 ]
 </script>
